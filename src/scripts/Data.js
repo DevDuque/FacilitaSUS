39a512c96fd4bfe5b90736843b0466297286b24b
Davih Duque <davihduque8@gmail.com>
2023-10-31 10:36:27 -0300

    Mudanças na aplicação, mesclando copy com main

--- src/scripts/Data.js
+++
@@ -1,9 +0,0 @@
-  // Obter o email armazenado no localStorage
-  const userEmail = localStorage.getItem('userEmail');
-
-  // Obter o nome armazenado no localStorage
-  const userName = localStorage.getItem('userName');
-  
-  // Usar o userEmail e userName conforme necessário
-  console.log(`Email: ${userEmail}`);
-  console.log(`Nome: ${userName}`);
