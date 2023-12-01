1c93f7e40debc2c9a5923f4aaac730f16def011c
Davih Duque <davihduque8@gmail.com>
2023-12-01 11:30:41 -0300

    Adicionando mensagens_UserID

--- src/scripts/Message.js
+++ src/scripts/Message.js
@@ -31,11 +31,24 @@ messageForm.addEventListener('submit', function(event) {
 });
 
 function salvarMensagemNoLocalStorage(usuarioLogado, mensagem) {
+    // Verifique se usuarioLogado é definido
+    if (!usuarioLogado) {
+        console.error('Erro: usuarioLogado é undefined.');
+        return;
+    }
+
     const usuarioInfo = JSON.parse(localStorage.getItem('usuarioLogado'));
+
+    // Certifique-se de que usuarioInfo e usuarioInfo.id são definidos
+    if (!usuarioInfo || !usuarioInfo.id) {
+        console.error('Erro: usuário ou ID do usuário é undefined.');
+        return;
+    }
+
     const usuarioId = usuarioInfo.id;
 
     // Obter mensagens salvas ou criar um novo array vazio
-    const mensagensSalvas = JSON.parse(localStorage.getItem(`mensagens_${usuarioLogado}`)) || [];
+    const mensagensSalvas = JSON.parse(localStorage.getItem(`mensagens_${usuarioId}`)) || [];
     console.log('Mensagens salvas:', mensagensSalvas);
 
     // Adicionar a mensagem ao array de mensagens
@@ -43,7 +56,7 @@ function salvarMensagemNoLocalStorage(usuarioLogado, mensagem) {
     mensagensSalvas.push(mensagem);
 
     // Salvar apenas as mensagens no localStorage, sem informações completas do usuário
-    localStorage.setItem(`mensagens_${usuarioLogado}`, JSON.stringify(mensagensSalvas.map(m => ({ ...m, usuario: { id: m.usuario.id } }))));
+    localStorage.setItem(`mensagens_${usuarioId}`, JSON.stringify(mensagensSalvas.map(m => ({ ...m, usuario: { id: m.usuario.id } }))));
 
     // Envie os dados para o Formsprее
     enviarFormularioParaFormspree(mensagem);
@@ -74,33 +87,45 @@ function enviarFormularioParaFormspree(mensagem) {
 }
 
 function carregarMensagens(usuarioLogado) {
-    const mensagensSalvas = JSON.parse(localStorage.getItem(`mensagens_${usuarioLogado}`)) || [];
+    // Parse do JSON para obter as informações do usuário
+    const usuarioInfo = JSON.parse(localStorage.getItem('usuarioLogado'));
+
+    if (!usuarioInfo || !usuarioInfo.id) {
+        console.error('Erro: usuário ou ID do usuário é undefined.');
+        return;
+    }
+
+    const usuarioId = usuarioInfo.id;
+
+    // Obter mensagens salvas ou criar um novo array vazio
+    const mensagensSalvas = JSON.parse(localStorage.getItem(`mensagens_${usuarioId}`)) || [];
     const sidebarDiv = document.querySelector('.sidebar');
     sidebarDiv.innerHTML = '';
 
-    mensagensSalvas.forEach(mensagem => {
-        const newMessageDiv = document.createElement('div');
-        newMessageDiv.classList.add('message');
+    mensagensSalvas
+        .forEach(mensagem => {
+            const newMessageDiv = document.createElement('div');
+            newMessageDiv.classList.add('message');
 
-        const subContentDiv = document.createElement('div');
-        subContentDiv.classList.add('sub-content');
+            const subContentDiv = document.createElement('div');
+            subContentDiv.classList.add('sub-content');
 
-        const tituloElement = document.createElement('h2');
-        tituloElement.textContent = formatarTitulo(mensagem.titulo);
-        subContentDiv.appendChild(tituloElement);
+            const tituloElement = document.createElement('h2');
+            tituloElement.textContent = formatarTitulo(mensagem.titulo);
+            subContentDiv.appendChild(tituloElement);
 
-        const tipoElement = document.createElement('h2');
-        tipoElement.textContent = mensagem.tipo;
-        subContentDiv.appendChild(tipoElement);
+            const tipoElement = document.createElement('h2');
+            tipoElement.textContent = mensagem.tipo;
+            subContentDiv.appendChild(tipoElement);
 
-        const conteudoElement = document.createElement('p');
-        conteudoElement.textContent = mensagem.conteudo;
+            const conteudoElement = document.createElement('p');
+            conteudoElement.textContent = mensagem.conteudo;
 
-        newMessageDiv.appendChild(subContentDiv);
-        newMessageDiv.appendChild(conteudoElement);
+            newMessageDiv.appendChild(subContentDiv);
+            newMessageDiv.appendChild(conteudoElement);
 
-        sidebarDiv.appendChild(newMessageDiv);
-    });
+            sidebarDiv.appendChild(newMessageDiv);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
